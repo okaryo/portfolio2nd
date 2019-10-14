31a2efd9550aa28a6c44b9ddad7c9da8e77afdc7
okaryo <okaryotr@gmail.com>
2019-10-14 12:28:14 +0900

    [add] social design to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,15 @@
 class ProfilesController < ApplicationController
   def show
+    @tags = [
+      'Twitter',
+      'Bookmeter',
+      'Jognote',
+      'Blog',
+      'Podcast',
+      'Github',
+      'Qiita',
+      'AtCoder',
+      'Wish List'
+    ]
   end
 end
